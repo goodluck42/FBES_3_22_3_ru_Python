41995c8c5ca11fab19fca882e1f403d4ba7d7c31
Alex <xrlogic1@gmail.com>
2022-12-09 21:58:01 +0400

    Update

---
+++ WhileLoop/main.py
@@ -0,0 +1,47 @@
+# i = 0
+
+# while i < 10000:
+#     try:
+#         if i == 100:
+#             print("i is equals 100")
+#     except:
+#         print("An error occurred")
+#
+#     i += 1  # инкремент (increment)
+#
+#
+# print(i)
+# print("Done")
+
+# i = 10
+#
+# while i > 0:
+#     print(i)
+#     i -= 1  # декремент (decrement)
+
+
+# i = 0
+#
+# while i < 10000:
+#     i += 1
+#
+
+
+# 3
+# i = 0
+#
+# print("First loop:")
+# while i < 10:
+#     print(i)
+#     i += 1
+#
+# i = 0
+# print("Second loop:")
+# while i < 15:
+#     print(i)
+#     i += 1
+
+
+# Диапазон: 1 до 20
+# Кратные 5: 5, 10, 15, 20
+# result: 4
